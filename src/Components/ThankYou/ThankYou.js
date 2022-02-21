e726fd6b7ba72f174eb742bc092e409ed6ef0c0c
harsh patel <harsh.1722.patel@gmail.com>
2022-02-21 14:16:28 +0530

    scrolltotop1

--- src/Components/ThankYou/ThankYou.js
+++ src/Components/ThankYou/ThankYou.js
@@ -7,29 +7,21 @@ const ThankYou = () => {
 
     // Top: 0 takes us all the way back to the top of the page
     // Behavior: smooth keeps it smooth!
-    const scrollToTop = () => {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-      });
-    };
-  
-    useEffect(() => {
-      // Button is displayed after scrolling for 500 pixels
-      const toggleVisibility = () => {
-        if (window.pageYOffset > 10) {
-          setIsVisible(true);
-        } else {
-          setIsVisible(false);
-        }
-      };
-  
-      window.addEventListener("scroll", toggleVisibility);
-  
-      return () => window.removeEventListener("scroll", toggleVisibility);
-    }, []);
 
-    scrollToTop();
+useEffect(()=>{
+
+    const scrollToTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+        });
+      };
+    
+
+})
+
+    
+
 
   return (
     <>
